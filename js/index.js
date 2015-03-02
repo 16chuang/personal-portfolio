0f85a8418119680aeb4c1918986650b414b4b7a3
16chuang <16chuang@castilleja.org>
2015-03-01 21:11:12 -0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+var welcome_callout = function () {
+    'use strict';
+    $("#welcome-callout").delay(2000).fadeIn(500);
+};
+
+var main = function () {
+    'use strict';
+    welcome_callout();
+};
+
+$(document).ready(main);
