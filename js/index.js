61d8133366e27470ba546c9f4b6183300e16b1ab
16chuang <16chuang@castilleja.org>
2015-11-25 21:55:25 -0800

    Updated about me page.

--- js/index.js
+++ js/index.js
@@ -4,27 +4,24 @@
 var main = function () {
     'use strict';
 
-    $('.expandable-panel').on('click', function() {
-        var $panel = $(this);
-        $panel.css('flex-grow', '5');
-
-        var $siblings = $panel.siblings()
-        $siblings.css('flex-grow', '1');
-    });
-    
-    // reset flex box if click outside of the panels
-    $(document).on('click', function(event) {
-        if(!$(event.target).closest('.expandable-panel').length) {
-            $('.expandable-panel').css('flex-grow', '1');
-        }
-    });
+//    // expand expandable panel div if clicked
+//    $('.expandable-panel').on('click', function() {
+//        var $panel = $(this);
+//        $panel.css('flex-grow', '5');
+//
+//        var $siblings = $panel.siblings()
+//        $siblings.css('flex-grow', '1');
+//    });
+//    
+//    // reset flex box if click outside of the panels
+//    $(document).on('click', function(event) {
+//        if(!$(event.target).closest('.expandable-panel').length) {
+//            $('.expandable-panel').css('flex-grow', '1');
+//        }
+//    });
      
     // instantiate Mix It Up
     $('#projects-container').mixItUp();
 };
 
 $(document).ready(main);
-
-//function expandPanelHandler() {
-//    window.alert("sometext");
-//}
