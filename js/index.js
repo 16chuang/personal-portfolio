cbae1bc0b5e4444116ea8e99a487f1bed8bc4341
16chuang <16chuang@castilleja.org>
2016-04-04 10:05:45 -0700

    another redesign of projects page

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
 
+var descriptionColorsDict = {
+    "mezzo":"37,94,54", "trext":"46,85,77", "bluestamp":"40,44,54", "sierra":"61,72,102",
+    "quakes":"52,102,112", "govo":"97,78,43", "catalan":"109,52,48"
+};
+
 var main = function () {
     'use strict';
      
@@ -8,26 +13,34 @@ var main = function () {
     $('#projects-container').mixItUp();
     
     $('div.mix').each(function(n) {
-        addBackgroundOverlay($(this), true);
+        var div = $(this);
+        setUpDiv(div);
+        showDescription(div, false);
     });
     
     $("div.mix").hover(function() {
-        var div = $(this);
-        div.children().css("display", "none");
-        addBackgroundOverlay(div, false);
+        showDescription($(this), true);
     }, function() {
-        var div = $(this);
-        div.children().css("display", "");
-        addBackgroundOverlay(div, true);
+        showDescription($(this), false);
     });
 };
 
-// adds or removes semi-transparent overlay on given div
-function addBackgroundOverlay(div, hide) {
+function showDescription(div, show) {
+    if (!show) { // normally hidden state
+        div.children('.description').animate({bottom:"-55px"},250);
+    } else { // hover show state
+        div.children('.description').animate({bottom:"0px"},250);
+    }
+}
+
+function setUpDiv(div) {
     var id = div.attr('id');
     var bkgdStr = "url(img/projects/pics/"+id+".png)";
-    if (hide) { bkgdStr = "linear-gradient(rgba(50, 50, 50, 0.7), rgba(50, 50, 50, 0.8))," + bkgdStr; }
     div.css("background", bkgdStr);
+    
+    var description = div.children('.description');
+//    description.css("background-color", descriptionColorsDict[id]);
+    description.css("background-color", "rgba("+descriptionColorsDict[id]+",.85)");
 }
 
 $(document).ready(main);
